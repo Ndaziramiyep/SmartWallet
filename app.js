933320dbf42999ab39f0d08320a54f1fb06d94d5
Ndaziramiyep <patrickofficial30@gmail.com>
2025-01-18 16:29:11 +0200

    coding for report.ejs

--- app.js
+++ app.js
@@ -3,24 +3,6 @@ const mysql = require("mysql");
 
 const app=express();
 // database
-CREATE DATABASE wallet;
-
-USE wallet;
-
-CREATE TABLE transactions (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    accountType VARCHAR(255) NOT NULL,
-    amount DECIMAL(10, 2) NOT NULL,
-    transactionType ENUM('income', 'expense') NOT NULL,
-    category VARCHAR(255),
-    subcategory VARCHAR(255),
-    date DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE budget (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    limit DECIMAL(10, 2) NOT NULL
-);
 
 
 
