e13662d5a426090adefaba9833e0afd27068e8ab
Ndaziramiyep <patrickofficial30@gmail.com>
2025-01-18 16:26:55 +0200

    adding files and folders

--- app.js
+++ app.js
@@ -2,6 +2,29 @@ const  express = require("express");
 const mysql = require("mysql");
 
 const app=express();
+// database
+CREATE DATABASE wallet;
+
+USE wallet;
+
+CREATE TABLE transactions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    accountType VARCHAR(255) NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    transactionType ENUM('income', 'expense') NOT NULL,
+    category VARCHAR(255),
+    subcategory VARCHAR(255),
+    date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE budget (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    limit DECIMAL(10, 2) NOT NULL
+);
+
+
+
+
 
 app.get('/', (req,res) =>{
 res.send("Hello Everyone!");
